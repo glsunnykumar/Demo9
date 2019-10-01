14e38e78a2f4d5e9a7cbf3c36c69a25de13a510f
Gulshan Kumar <glsunny.kumar@gmail.com>
2019-10-01 14:26:55 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  title = 'Calculator App';
+  num1 : number;
+  num2 : number;
+  result : number;
+
+
+  add(){
+    this.result = this.num1 + this.num2;
+  }
+
+  substract()
+  {
+    this.result = this.num1 - this.num2;
+  }
+
+  Multiply(){
+    this.result = this.num1 * this.num2;
+  }
+
+  Divide(){
+    this.result = this.num1 / this.num2;
+  }
+
 }
